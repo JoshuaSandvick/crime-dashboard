a8aa245730ad094f6ab65106edc7f237bfe47e20
JoshuaSandvick <josh.sandvick@gmail.com>
2022-03-12 20:49:16 -0600

    More tutorial popover fixes for small screens

--- client/src/Components/Dashboard/TutorialPopover.tsx
+++ client/src/Components/Dashboard/TutorialPopover.tsx
@@ -10,27 +10,48 @@ export interface TutorialPopoverProps {
 
 const TutorialPopover: React.FC<TutorialPopoverProps> = (props) => {
     const { anchorElement, text, open } = props;
+    const [arrowPopoverRef, setArrowPopoverRef] = React.useState<Element>();
 
     return (
-        <Popover
-            open={open}
-            anchorEl={anchorElement}
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-            elevation={0}
-            PaperProps={{ sx: { backgroundColor: 'transparent' } }}
-        >
-            <Stack direction="column" alignItems="center">
-                <ArrowUpIcon sx={{ color: 'white', padding: '1px' }} />
-                <Typography
-                    variant="h5"
-                    sx={{ color: 'white', fontStyle: 'italic', padding: '5px' }}
-                    align="center"
-                >
-                    {text}
-                </Typography>
-            </Stack>
-        </Popover>
+        <>
+            <Popover
+                open={open}
+                anchorEl={anchorElement}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+                elevation={0}
+                PaperProps={{ sx: { backgroundColor: 'transparent' } }}
+            >
+                <Stack direction="column" alignItems="center">
+                    <ArrowUpIcon
+                        sx={{ color: 'white' }}
+                        ref={(newRef) => {
+                            if (newRef) {
+                                setArrowPopoverRef(newRef);
+                            }
+                        }}
+                    />
+                </Stack>
+            </Popover>
+            <Popover
+                open={open}
+                anchorEl={arrowPopoverRef}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+                elevation={0}
+                PaperProps={{ sx: { backgroundColor: 'transparent' } }}
+            >
+                <Stack direction="column" alignItems="center">
+                    <Typography
+                        variant="h5"
+                        sx={{ color: 'white', fontStyle: 'italic', padding: '5px' }}
+                        align="center"
+                    >
+                        {text}
+                    </Typography>
+                </Stack>
+            </Popover>
+        </>
     );
 };
 
